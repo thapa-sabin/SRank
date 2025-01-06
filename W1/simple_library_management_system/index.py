1a53a87e814e74f988628befc5ea6b1634de1a86
thapa-sabin <thapasabin@zohomail.com>
2025-01-06 20:24:33 +0545

    Made changes to some variable naming schemes and added a feature to allow members to borrow more than one book.

--- W1/simple_library_management_system/index.py
+++ W1/simple_library_management_system/index.py
@@ -1,5 +1,4 @@
 class Book:
-    # I think using triple quotes would be better here.
     """Keeps track of borrowed books"""
     def __init__(self, title, author, availability=True):
         self.title = title
@@ -9,43 +8,84 @@ class Book:
     def borrow(self):
         if self.availability:
             self.availability = False
-            return f"Borrowed {self.title} by {self.author}."
+            return f"{self.title} by {self.author} was borrowed successfully."
         else:
             return "Book unavailable."
     
     def return_book(self):
         self.availability = True
-        return f"{self.title} by {self.author} returned successfully."
+        return f"{self.title} by {self.author} was returned successfully."
 
 class LibraryMember:
-    """Identifies members based on the ID and book they have borrrowed."""
-    def __init__(self, name, ID):
-        self.name = name
-        self.ID = ID
+    """Identifies members based on their unique ID and books they have borrowed"""
+    def __init__(self, name, uid):
+       self.name = name
+       self.uid = uid
+       self.borrowed_books = [] # Keeping track of which books a member has borrowed
     
     def borrow_a_book(self, book, library):
-        return library.borrow_a_book(book)
+        if book in self.borrowed_books:
+            return f"You have already borrowed {book.title} by {book.author}. Please choose a different one."
+        borrow_message = library.borrow_a_book(book)
+
+        if "borrowed" in borrow_message:
+            self.borrowed_books.append(book)
+        return borrow_message
     
     def return_book(self, book, library):
-        return library.return_book(book)
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+            return library.return_book(book)
+        else:
+            return f"Someone else already has the book {book.title} by {book.title}."
 
 class Library:
     def __init__(self):
-        self.books_here = [] # Keeping track of books in the library
-        self.members_here = [] # Record of all Library members
-    
+        self.available_books = [] # Keeping track of books in the Library with a list
+        self.member_list = [] # Keeping track of all members in the library
+
     def add_book(self, book):
-        self.books_here.append(book)
+        self.available_books.append(book)
     
     def add_member(self, member):
-        self.members_here.append(member)
-
+        self.member_list.append(member)
+    
     def borrow_a_book(self, book):
-        if book in self.books_here and book.availability:
+        if book in self.available_books and book.availability:
             return book.borrow()
         else:
-            return "Book not available."
-    
+            return "Book unavailable."
+        
     def return_book(self, book):
         book.return_book()
         return f"{book.title} by {book.author} has been returned to the library."
+
+# Testing my updated code
+
+# Adding some books (From Project Gutenberg) and members to the library
+book1 = Book("Amelia", "Henry Fielding")
+book2 = Book("Aesop's Fables - Volume 01", "Chesterton")
+book3 = Book("Pride and Prejudice", "Jane Austen")
+member1 = LibraryMember("Sabin Thapa", 123)
+member2 = LibraryMember("Sushi Marasini", 456)
+
+# Creating an instance of the class Library
+library = Library()
+
+# Adding sample books and members to the library
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+library.add_member(member1)
+library.add_member(member2)
+
+# Testing borrowing and returning features
+print(member1.borrow_a_book(book1, library))  # Borrow book1
+print(member1.borrow_a_book(book2, library))  # Borrow book2
+print(member1.borrow_a_book(book1, library))  # Try borrowing the same book again
+
+print(member1.return_book(book1, library))    # Return book1
+print(member1.borrow_a_book(book1, library))  # Borrow book1 again after returning
+
+print(member2.borrow_a_book(book3, library))  # Member2 borrows book3
+print(member2.return_book(book3, library))    # Member2 returns book3
