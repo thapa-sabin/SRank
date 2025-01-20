045beed546f1939703b10a5b3d052b7faecd9259
thapa-sabin <thapasabin@zohomail.com>
2025-01-20 16:47:57 +0545

    Using Tailwind CSS to style the elements.

--- W2/todo_react/src/index.js
+++ W2/todo_react/src/index.js
@@ -1,11 +1,11 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client'
-
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 import './index.css';
 import App from './App';
 
-// Create root element
-const root = ReactDOM.createRoot(document.getElementById('root')); 
+// Create a root and render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
